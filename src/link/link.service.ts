2fcd6132c1d6bd155cb11179fc12673fbbf4414c
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-11-26 22:45:20 +0100

    Add getById and Create methods to LinkService

--- src/link/link.service.ts
+++ src/link/link.service.ts
@@ -1,12 +1,28 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import CreateLinkReq from './dtos/CreateLinkReq.dto';
+import { Link, LinkDocument } from './schemas/Link.schema';
 import ScrapService from './scrap.service';
 
 @Injectable()
 export class LinkService {
-	constructor(@Inject(ScrapService) private scrapService: ScrapService) {}
+	constructor(
+		@Inject(ScrapService) private scrapService: ScrapService,
+		@InjectModel(Link.name) private linkModel: Model<LinkDocument>,
+	) {}
 
-	async saveUrl(dto: CreateLinkReq) {
-		return await this.scrapService.getOgData(dto.url);
+	async getById(id:string){
+		const link = await this.linkModel.findById(id);
+		if(!link) throw new Error("Not found")
+		return link
+	}
+
+	async create(dto: CreateLinkReq) {
+		const existingLink = await this.linkModel.findOne({url: dto.url})
+		if(existingLink) return existingLink
+		const data = await this.scrapService.getOgData(dto.url);
+		const createdLink = new this.linkModel(data);
+		return createdLink.save();
 	}
 }
