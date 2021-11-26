3330c2dd473d981cadece9345d51119bdf264308
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-11-26 22:43:55 +0100

    Add link model

--- src/link/link.module.ts
+++ src/link/link.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import ScrapService from './scrap.service';
 import { LinkController } from './link.controller';
 import { LinkService } from './link.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Link, LinkSchema } from './schemas/Link.schema';
 
 @Module({
+	imports: [
+		MongooseModule.forFeature([
+			{
+				name: Link.name,
+				schema: LinkSchema,
+			},
+		]),
+	],
 	controllers: [LinkController],
 	providers: [ScrapService, LinkService],
 })
---
+++ src/link/schemas/Link.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type LinkDocument = Link & Document;
+
+@Schema()
+export class Link {
+	@Prop({ required: true })
+	url: string;
+
+	@Prop({ required: true })
+	title: string;
+
+	@Prop({ required: true })
+	description: string;
+
+	@Prop({ required: true })
+	siteName: string;
+
+	@Prop()
+	imageUrl: string;
+
+	@Prop({default: false})
+	read: boolean;
+
+	@Prop({default: false})
+	archived: boolean
+}
+
+export const LinkSchema = SchemaFactory.createForClass(Link);
