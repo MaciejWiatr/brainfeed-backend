c0ebb9c19904027994b05f1c623b8ea196d7f7d5
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-12-05 22:48:24 +0100

    Add simple caching strategy

--- src/link/link.controller.ts
+++ src/link/link.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import {
+	Body,
+	CacheInterceptor,
+	Controller,
+	Get,
+	Inject,
+	Param,
+	Post,
+	UseInterceptors,
+} from '@nestjs/common';
 import CreateLinkReq from './dtos/CreateLinkReq.dto';
 import { LinkService } from './link.service';
 
@@ -6,6 +15,7 @@ import { LinkService } from './link.service';
 export class LinkController {
 	constructor(@Inject(LinkService) private urlService: LinkService) {}
 
+	@UseInterceptors(CacheInterceptor)
 	@Get('/:id')
 	async getLinkById(@Param('id') id: string) {
 		return this.urlService.getById(id);
